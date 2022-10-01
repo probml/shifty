e7eb93357439253dbc5507b6f9434f0134486238
Kevin Murphy <murphyk@gmail.com>
2022-10-01 21:33:17 +0000

    WIP

--- shifty/label_shift/tta_gauss.py
+++ shifty/label_shift/tta_gauss.py
@@ -23,122 +23,105 @@ from tensorflow_probability.substrates import jax as tfp
 tfd = tfp.distributions
 from tensorflow_probability.substrates.jax.distributions import MultivariateNormalFullCovariance as MVN
 
+# Model from NURD paper:
+# A. M. Puli, L. H. Zhang, E. K. Oermann, and R. Ranganath, 
+# “Out-of-distribution Generalization in the Presence of Nuisance-Induced Spurious Correlations,” 
+#  ICLR, May 2022 [Online]. Available: https://openreview.net/forum?id=12RoR2o32T.
 
-NCLASSES = 2
-NFACTORS = 2 # nuisance factors
-NMIX = NCLASSES * NFACTORS # mixture components
-NDIM = 2 # dimensionality of observed data
-
-def yz_to_mix(y, z):
-    m = jnp.ravel_multi_index((jnp.array([y]), jnp.array([z])), (NCLASSES, NFACTORS))
-    return m[0]
-
-def mix_to_yz(m):
-    yz = jnp.unravel_index(m, (NCLASSES, NFACTORS))
-    return yz[0], yz[1]
 
 @chex.dataclass
-class GenParams:
-    b: float # strength of dependence of x on z (nuisance factor)
-    sf: float # scale factor for Gaussian variance
+class NurdParams:
+    b: float = 1 # strength of dependence of x on z (nuisance factor)
+    sf: float = 1 # scale factor for Gaussian variance
+    generator: str = "nurd"
 
-def class_cond_density_nurd(y, z, b, sf):  
-    # returns parameters for distribution p(x|y,z)
-    # Model from NURD paper:
-    # A. M. Puli, L. H. Zhang, E. K. Oermann, and R. Ranganath, 
-    # “Out-of-distribution Generalization in the Presence of Nuisance-Induced Spurious Correlations,” 
-    #  ICLR, May 2022 [Online]. Available: https://openreview.net/forum?id=12RoR2o32T. 
+@chex.dataclass
+class DiscrimParams:
+    poly_degree: int = 2
+    max_iter: int = 100
+
+@chex.dataclass
+class MetaParams: # these determine the sizeshape of the model
+    ndim: float = 2 # dimensionality of generated X data
+    nclasses: int = 2
+    nfactors: int = 2 # other attributes
+    nmix: int = 4 # nclasses * nfactors
+
+
+def class_cond_params_nurd(m, nurd_params, meta_params):  
+    # returns parameters for distribution p(x|m=(y,z))
+    yz = jnp.unravel_index(m, (meta_params.nclasses, meta_params.nfactors))
+    y, z = yz[0], yz[1]
+    b, sf = nurd_params.b, nurd_params.sf
     ysigned, zsigned = 2.0*y-1,  2.0*z-1 #   # convert from (0,1) to (-1,1)
     mu = jnp.array([ysigned - b*zsigned, ysigned + b*zsigned])
     Sigma = sf*jnp.diag(jnp.array([1.5, 0.5]))
     return mu, Sigma
 
-def make_lik_dist(m, gen_params): # deprecated
-    y, z = mix_to_yz(m)
-    mu, Sigma = class_cond_density_nurd(y, z, gen_params.b, gen_params.sf)
-    dist = MVN(loc = mu, covariance_matrix = Sigma)
-    return dist
-
-def lik_fn_old(m, X, gen_params): # deprecated
+def lik_fn_nurd(m, X, nurd_params, meta_params):
     # returns p(X(n,:) | m) as (N,1) vector
-    dist = make_lik_dist(m, gen_params)
-    return dist.prob(X)
+    mu, Sigma = class_cond_params_nurd(m, nurd_params, meta_params)
+    return jsp.stats.multivariate_normal.pdf(X, mu, Sigma)
 
-def lik_fn(m, X, gen_params):
-    # returns p(X(n,:) | m) as (N,1) vector
-    y, z = mix_to_yz(m)
-    mu, Sigma = class_cond_density_nurd(y, z, gen_params.b, gen_params.sf)
-    return jsp.multivariate_normal.prob(X, mu, Sigma)
-
-def make_mix_dist(prior, gen_params): # deprecated
-    def f(m):
-        dist = make_lik_dist(m, gen_params)
-        return dist.mean(), dist.covariance()
-    mus, Sigmas = vmap(f)(jnp.arange(NMIX))
-    dist = tfd.MixtureSameFamily(
-      mixture_distribution=tfd.Categorical(probs=prior),
-      components_distribution=MVN(loc = mus, covariance_matrix = Sigmas))
-    joint_dist = tfd.JointDistributionSequential(
-        [tfd.Categorical(logits=jnp.log(prior)), 
-        lambda label: MVN(loc=mus[label], covariance_matrix=Sigmas[label])])
-    return dist, joint_dist
-
-def sample_data_old(key, prior, gen_params, nsamples): # deprecated
-    labels =  jr.categorical(key, logits=jnp.log(prior), shape=(nsamples,))
-    X = jnp.zeros((nsamples, NDIM))
-    for m in range(NMIX):
-        ndx = jnp.nonzero(labels==m)[0]
-        n = len(ndx) # num. samples for this mixture component
-        dist = make_lik_dist(m, gen_params)
-        samples = dist.sample(seed=key, sample_shape=n)
-        X = X.at[ndx].set(samples)
-    return X, labels
-
-def sample_data(key, prior, gen_params, nsamples):
+def sample_data_nurd(key, nsamples, prior, nurd_params, meta_params):
     labels = jr.categorical(key, logits=jnp.log(prior), shape=(nsamples,))
     def f(m):
-        y, z = mix_to_yz(m)
-        mu, Sigma = class_cond_density_nurd(y, z, gen_params.b, gen_params.sf)
-        return mu, Sigma
-    mus, Sigmas = vmap(f)(jnp.arange(NMIX))
+        return class_cond_params_nurd(m, nurd_params, meta_params)
+    mus, Sigmas = vmap(f)(jnp.arange(meta_params.nmix))
     X = jr.multivariate_normal(key, mus[labels], Sigmas[labels])
     return X, labels
 
-def make_prior(rho):
+
+def make_prior(rho, meta_params):
     p_class =  np.array([0.5, 0.5]) # uniform prior  on p(y) = (-1, +1)
     p_factor_given_class = np.zeros((2, 2))  # p(z|c) = p_factor(c,z) so each row sums to 1
     p_factor_given_class[0, :] = [1 - rho, rho]
     p_factor_given_class[1, :] = [rho, 1 - rho]
-    p_mix = np.zeros((NCLASSES, NFACTORS))  # (c,z)
-    for c in range(NCLASSES):
-        for z in range(NFACTORS):
-            p_mix[c, z] = p_class[c] * p_factor_given_class[c, z]
-    p_mix = einops.rearrange(p_mix, 'y z -> (y z)')
-    return p_mix
+    pmix = jnp.einsum('y,yz->yz', p_class, p_factor_given_class)
+    pmix = einops.rearrange(pmix, 'y z -> (y z)')
+    return pmix
 
-def predict_bayes(prior, lik_fn, X, gen_params):  
-    liks = vmap(partial(lik_fn, X=X, gen_params=gen_params))(jnp.arange(NMIX)) # (K,N)
-    joint = jnp.einsum('kn,k -> nk', liks, prior)
-    norm = joint.sum(axis=1)
-    mix_post = joint / jnp.expand_dims(norm, axis=1)
-    mix_post = einops.rearrange(mix_post, 'n (y z) -> n y z', y=NCLASSES, z=NFACTORS)
+def compute_class_post_from_joint(mix_post, meta_params):
+    mix_post = einops.rearrange(mix_post, 'n (y z) -> n y z', y=meta_params.nclasses, z=meta_params.nfactors)
     class_post = einops.reduce(mix_post, 'n y z -> n y', 'sum') 
     return class_post
 
+def predict_bayes(prior, lik_fn, X, meta_params):  
+    liks = vmap(partial(lik_fn, X=X))(jnp.arange(meta_params.nmix)) # (K,N)
+    joint = jnp.einsum('kn,k -> nk', liks, prior) # joint(n,k) = liks(k,n) * prior(k)
+    norm = joint.sum(axis=1)
+    joint_post = joint / jnp.expand_dims(norm, axis=1) # joint_post(n,k) = p(mix = k | xn)
+    class_post = compute_class_post_from_joint(joint_post, meta_params)
+    return class_post
 
-def predict_source(classifier, x):
-    return classifier.proba(x)
+def predict_source(classifier, X, meta_params):
+    joint_post = classifier.predict_proba(X) 
+    return compute_class_post_from_joint(joint_post, meta_params)
 
-def classifier_to_lik_fn(classifier):
-    pass
+def fit_source(key, X, joint_labels, discrim_params):
+    classifier = Pipeline([
+            ('standardscaler', StandardScaler()),
+            ('poly', PolynomialFeatures(degree=discrim_params.poly_degree)), 
+            ('logreg', LogisticRegression(random_state=0, max_iter=discrim_params.max_iter))])
+    classifier.fit(X, joint_labels)
+    probs_joint = classifier.predict_proba(X) # (n,m)
+    prior_joint = jnp.mean(probs_joint, axis=0) # prior(m) = empirical fraction of times m is predicted
+    return classifier, prior_joint
 
-def predict_target(prior, classifier, x):
+
+def classifier_to_lik_fn(classifier, prior):
+    def lik_fn(m, X):
+        # return p_s(x(n) | m) = p_s(m|x) p_s(x) / p_s(m) propto p_s(m|x) / p_s(m)
+        probs = jnp.array(classifier.predict_proba(X))
+        probs = probs[:,m] / prior[m]
+        return probs
+    return lik_fn
+
+def predict_target(prior, classifier, X, meta_params):
     lik_fn = classifier_to_lik_fn(classifier)
-    return predict_bayes(prior, lik_fn, x)
+    return predict_bayes(prior, lik_fn, X, meta_params)
 
-def fit_source(key, labeled_data):
-    model = None
-    return model
+
 
 def fit_target(key, model, unlabeled_data):
     #model = update(model, unlabeled_data)
