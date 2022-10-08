fd9fc53b73e81dc845a5a111fa9fc4c11ddcb3e6
Kevin Murphy <murphyk@gmail.com>
2022-10-08 19:35:27 +0000

    warmup

--- shifty/skax/skax.py
+++ shifty/skax/skax.py
@@ -39,24 +39,11 @@ import tensorflow_datasets as tfds
 
 from sklearn.base import ClassifierMixin
 
-def loglikelihood_fn(params, model, X, y):
-    # 1/N sum_n log p(yn | xn, params)
-    logits = model.apply(params, X)
-    return jnp.mean(distrax.Categorical(logits).log_prob(y))
-
 def logprior_fn(params, sigma):
     # log p(params)
     leaves, _ = jax.tree_util.tree_flatten(params)
     flat_params = jnp.concatenate([jnp.ravel(a) for a in leaves])
     return jnp.sum(distrax.Normal(0, sigma).log_prob(flat_params))
-
-@partial(jax.jit, static_argnames=["network"])
-def objective(params, data, network, prior_sigma, ntrain): 
-    # objective = -1/N [ (sum_n log p(yn|xn, theta)) + log p(theta) ]
-    X, y = data["X"], data["y"]
-    logjoint = loglikelihood_fn(params, network, X, y) + (1/ntrain)*logprior_fn(params, prior_sigma)
-    return -logjoint
-
 
 def make_batched_dataset(X, y, batch_size):
     # Convert dataset into a fixed set of minibatches 
@@ -73,11 +60,21 @@ def make_data_iterator(X, y, batch_size):
     ds = tf.data.Dataset.from_tensor_slices({"X": X, "y": y})
     # https://jaxopt.github.io/stable/auto_examples/deep_learning/haiku_image_classif.htm
     ds = ds.cache().repeat()
-    #ds = ds.shuffle(10 * batch_size, seed=0) # how use key?
-    ds = ds.shuffle(ntrain)
+    ds = ds.shuffle(10 * batch_size, seed=0) # how use key?
+    #ds = ds.shuffle(ntrain)
     ds = ds.batch(batch_size)
     iterator = iter(tfds.as_numpy(ds)) #ds.as_numpy_iterator()
     return iterator
+
+@partial(jax.jit, static_argnums=(1,2))
+def get_batch_train_ixs(key, num_train, batch_size):
+    # return indices of training set in a random order
+    steps_per_epoch = num_train // batch_size
+    batch_ixs = jax.random.permutation(key, num_train)
+    batch_ixs = batch_ixs[:steps_per_epoch * batch_size]
+    batch_ixs = batch_ixs.reshape(steps_per_epoch, batch_size)
+    return batch_ixs
+
 
 class LogRegNetwork(nn.Module):
     nclasses: int
@@ -102,56 +99,79 @@ class MLPNetwork(nn.Module):
 
 
 class NeuralNetClassifier(ClassifierMixin):
-    def __init__(self, network, key, nclasses, *,  l2reg=1e-5,
-                optimizer = 'lbfgs', batch_size=128, max_iter=1000, num_epochs=5):
+    def __init__(self, network, key, nclasses, *,  l2reg=1e-5, standardize = True,
+                optimizer = 'lbfgs', batch_size=128, max_iter=1000, num_epochs=10, print_every=5):
         # optimizer is {'lbfgs', 'polyak', 'armijo'} or an optax object
         self.nclasses = nclasses
         self.network = network
-        self.optimization_results = None
+        self.standardize = standardize
         self.max_iter = max_iter
         self.num_epochs = num_epochs
         self.optimizer = optimizer
         self.batch_size = batch_size
         self.l2reg = l2reg
-        #x = jr.normal(key, (ninputs,)) # single random input 
-        #self.params = self.network.init(key, x)
+        self.print_every = print_every
         self.params = None # must first call fit
         self.key = key
 
-    def predict(self, inputs):
-        return jax.nn.softmax(self.network.apply(self.params, inputs))
+    def predict(self, X):
+        if self.standardize:
+            X = X - self.mean
+            X = X / self.std
+        return jax.nn.softmax(self.network.apply(self.params, X))
 
     def fit(self, X, y):
-        ninputs = X.shape[1]
-        x = jr.normal(self.key, (ninputs,)) # single random input 
-        self.params = self.network.init(self.key, x)
+        if self.params is None:
+            ninputs = X.shape[1]
+            x = jr.normal(self.key, (ninputs,)) # single random input 
+            self.params = self.network.init(self.key, x)
+        if self.standardize:
+            self.mean = jnp.mean(X, axis=0)
+            self.std = jnp.std(X, axis=0) + 1e-5 
+            X = X - self.mean
+            X = X / self.std
+        ntrain = X.shape[0]
         if isinstance(self.optimizer, str) and (self.optimizer.lower() == "lbfgs"):
             return self.fit_bfgs(self.key, X, y)
-        else:
+        elif isinstance(self.optimizer, str) and (self.optimizer.lower() == "polyak"):
             return self.fit_jaxopt(self.key, X, y)
+        elif isinstance(self.optimizer, str) and (self.optimizer.lower() == "armijo"):
+            return self.fit_jaxopt(self.key, X, y)
+        elif isinstance(self.optimizer, str) and (self.optimizer.lower() == "adam+warmup"):
+            total_steps = self.num_epochs*(ntrain//self.batch_size)  
+            warmup_cosine_decay_scheduler = optax.warmup_cosine_decay_schedule(
+                init_value=1e-3, peak_value=1e-1, warmup_steps=int(total_steps*0.1),
+                decay_steps=total_steps, end_value=1e-3)
+            self.optimizer = optax.adam(learning_rate=warmup_cosine_decay_scheduler)
+            return self.fit_optax(self.key, X, y)
+        else:
+            #return self.fit_jaxopt(self.key, X, y)
+            return self.fit_optax(self.key, X, y)
 
     def fit_bfgs(self, key, X, y):
         # Full batch optimization
         sigma = np.sqrt(1/self.l2reg)
-        N = X.shape[0]
-        data = {"X": X, "y": y}
+        ntrain = X.shape[0]
+        @jax.jit
         def loss_fn(params):
-            return objective(params=params, data=data,  network=self.network,  prior_sigma=sigma, ntrain=N)
+            # objective = -1/N [ (sum_n log p(yn|xn, theta)) + log p(theta) ]
+            logits = self.network.apply(params, X)
+            loglik = jnp.mean(distrax.Categorical(logits).log_prob(y))
+            logjoint = loglik + (1/ntrain)*logprior_fn(params, sigma)
+            return -logjoint
         solver = jaxopt.LBFGS(fun=loss_fn, maxiter=self.max_iter)
         res = solver.run(self.params)
         self.params = res.params
-        self.optimization_results = res
-
 
     def fit_jaxopt(self, key, X, y):
         # https://jaxopt.github.io/stable/auto_examples/deep_learning/flax_resnet.html
-        # https://github.com/blackjax-devs/blackjax/discussions/360#discussioncomment-3756412
         sigma = np.sqrt(1/self.l2reg)
-        ntrain, B = X.shape[0], self.batch_size
+        ntrain = X.shape[0]
 
         @jax.jit
         def loss_fn(params, data):
             # objective = -1/N [ (sum_n log p(yn|xn, theta)) + log p(theta) ]
+            # https://github.com/blackjax-devs/blackjax/discussions/360#discussioncomment-3756412
             X, y = data["X"], data["y"]
             logits = self.network.apply(params, X)
             loglik = jnp.mean(distrax.Categorical(logits).log_prob(y))
@@ -174,36 +194,41 @@ class NeuralNetClassifier(ClassifierMixin):
         # https://github.com/google/flax/blob/main/examples/vae/train.py
         sigma = np.sqrt(1/self.l2reg)
         ntrain = X.shape[0]
-        ds = make_data_iterator(X, y, self.batch_size)
-
-        state = train_state.TrainState.create(
-            apply_fn=self.network.apply, params=self.params['params'], tx=self.optimizer)
-
+ 
         @jax.jit
-        def train_step(state, data):
-            X, y = data["X"], data["y"]
+        def train_step(key, state, Xb, yb):
             # Computes gradients, loss and accuracy for a single batch.
             # loss = -1/N [ (sum_n log p(yn|xn, theta)) + log p(theta) ]
             def loss_fn(params):
-                logits = state.apply_fn({'params': params}, X)
-                loglik = jnp.mean(distrax.Categorical(logits).log_prob(y))
+                logits = state.apply_fn({'params': params}, Xb)
+                loglik = jnp.mean(distrax.Categorical(logits).log_prob(yb))
                 logjoint = loglik + (1/ntrain)*logprior_fn(params, sigma)
-                loss = -logjoint
-                return loss
-            loss, grads = jax.grad(loss_fn, has_aux=True)(state.params)
-            return state.apply_gradients(grads=grads), loss
+                return -logjoint
+            loss, grads = jax.value_and_grad(loss_fn)(state.params)
+            return loss, state.apply_gradients(grads=grads)
+
+        def train_epoch(key, state):
+            key, sub_key = jr.split(key)
+            batch_ixs = get_batch_train_ixs(sub_key, ntrain, self.batch_size)
+            num_batches = len(batch_ixs)
+            key, sub_key = jr.split(key)
+            keys = jax.random.split(sub_key, num_batches)    
+            total_loss = 0
+            for key, batch_ix in zip(keys, batch_ixs):
+                X_batch, y_batch = X[batch_ix], y[batch_ix]
+                loss, state = train_step(key, state, X_batch, y_batch)
+                total_loss += loss
+            return total_loss.item(), state
+
 
         # main loop
-        steps_per_epoch = ntrain / self.batch_size
+        state = train_state.TrainState.create(
+            apply_fn=self.network.apply, params=self.params['params'], tx=self.optimizer)
         for epoch in range(self.num_epochs):
-            epoch_loss = []
-            for _ in range(steps_per_epoch):
-                batch = next(ds)
-                #rng, key = random.split(rng)
-                state, loss = train_step(state, batch)  
-                epoch_loss.append(loss)
-            train_loss = np.mean(epoch_loss)
-            print('epoch {:d}, train loss {:0.3f}'.format(epoch, train_loss))
+            key, sub_key = jr.split(key)
+            train_loss, state = train_epoch(sub_key, state)
+            if (self.print_every > 0) and (epoch % self.print_every == 0):
+                print('epoch {:d}, train loss {:0.3f}'.format(epoch, train_loss))
 
         self.params = {'params': state.params}
 
--- shifty/skax/skax_test.py
+++ shifty/skax/skax_test.py
@@ -41,63 +41,78 @@ from sklearn.linear_model import LogisticRegression
 from skax import *
 #jax.config.update("jax_enable_x64", True) # jaxopt.lbfgs uses float32
 
+def print_probs(probs):
+    str = ['{:0.3f}'.format(p) for p in probs]
+    print(str)
 
-def make_test_data(key):
-    iris = sklearn.datasets.load_iris()
-    X = iris["data"]
-    #y = (iris["target"] == 2).astype(np.int)  # 1 if Iris-Virginica, else 0'
-    y = iris["target"]
-    nclasses = len(np.unique(y)) # 
-    ndata, ndim = X.shape  # 150, 4
-    key = jr.PRNGKey(0)
-    noise = jr.normal(key, (ndata, ndim)) * 2.0
-    X = X + noise # add noise to make the classes less separable
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    X = np.array(X)
-    perm = jr.permutation(key, ndata)
-    X, y = X[perm], y[perm]
+def make_random_data(seed, n_samples, class_sep=0.5, n_features=20):
+    X, y = sklearn.datasets.make_classification(n_samples=500, n_features=n_features,  n_informative=10,
+    n_redundant=5, n_repeated=0,
+    n_classes=10, n_clusters_per_class=1, weights=None, flip_y=0.01, class_sep=class_sep,
+    hypercube=True, shift=0.0, scale=1.0, shuffle=True, random_state=seed)
     return X, y
 
+
+
 def compute_mle(X, y):
-    # We set C to a large number to turn off regularization.
-    # We don't fit the bias term to simplify the comparison below.
-    log_reg = LogisticRegression(solver="lbfgs", C=1e5, fit_intercept=True)
-    log_reg.fit(X, y)
-    W_mle = log_reg.coef_ # (nclasses, ndim)
-    b_mle = log_reg.intercept_ # (nclasses,)
-    true_probs = log_reg.predict_proba(X)
-    return true_probs, W_mle, b_mle
-
-
-def compare_method(optimizer, name=None, batch_size=None, max_iter=500):
-    key = jr.PRNGKey(0)
-    X, y = make_test_data(key)
-    true_probs, W_mle, b_mle = compute_mle(X, y)
-    nclasses, ndim = W_mle.shape
+    # Use sklearn to fit logreg model
     l2reg = 1e-5
-    #network = LogRegNetwork(nclasses = nclasses)
+    classifier = Pipeline([
+            ('standardscaler', StandardScaler()), 
+            ('logreg', LogisticRegression(random_state=0, max_iter=100, C=1/l2reg))])
+    classifier.fit(X, y)
+    true_probs = classifier.predict_proba(X)
+    return true_probs
+
+
+def compare_method(optimizer, name=None, batch_size=None, num_epochs=50,
+                    data_seed=0, n_samples=500, class_sep=0.5):
+    key = jr.PRNGKey(0)
+    X, y = make_random_data(data_seed, n_samples, class_sep)
+    true_probs = compute_mle(X, y)
+    nclasses = len(np.unique(y))
+    if batch_size is None:
+        batch_size = n_samples # full batch
+    l2reg = 1e-5
     #network = MLPNetwork((5, nclasses,))
-    network = MLPNetwork((nclasses,)) # no hidden layers
-    model = NeuralNetClassifier(network, key, nclasses, l2reg=l2reg, optimizer = optimizer, batch_size=batch_size, max_iter=max_iter) 
-    #model = LogReg(key, nclasses, max_iter=max_iter, l2reg=l2reg, optimizer=optimizer, batch_size=batch_size)  
+    network = MLPNetwork((nclasses,)) # no hidden layers == logistic regression
+    model = NeuralNetClassifier(network, key, nclasses, l2reg=l2reg, optimizer = optimizer, 
+            batch_size=batch_size, num_epochs=num_epochs, print_every=0)  
     model.fit(X, y)
     probs = np.array(model.predict(X))
     print('method {:s}, max deviation from true probs {:.3f}'.format(name, np.max(true_probs - probs)))
-    print('truth: ', true_probs[0])
-    print('pred: ', probs[0])
+    print('truth'); print_probs(true_probs[0])
+    print('pred'); print_probs(probs[0])
 
 
-def test_bfgs():
+def s_test_bfgs():
     compare_method("lbfgs", name="lbfgs")
 
-def test_adam_full_batch():
-    X, y = make_test_data()
-    ntrain = X.shape[0]
-    compare_method(optax.adam(1e-3), name="adam 1e-3, bs=N", batch_size=ntrain)
+def s_test_armijo():
+    compare_method("armijo", name="armijo, bs=32", batch_size=32)
 
-def test_adam():
+def s_test_adam_const():
     compare_method(optax.adam(1e-3), name="adam 1e-3, bs=32", batch_size=32)
 
+def test_adam_warmup():
+    compare_method("adam+warmup", name="adam+warmup, bs=32", batch_size=32)
 
-def test_armijo():
-    compare_method("armijo", name="armijo, bs=32", batch_size=32)
+def test_adam_warmup1():
+    data_seed=1; n_samples=500; class_sep=0.5
+    compare_method("adam+warmup", name="adam+warmup, bs=32", batch_size=32,
+        data_seed=data_seed, n_samples=n_samples, class_sep=class_sep)
+
+def test_adam_warmup2():
+    data_seed=1; n_samples=500; class_sep=1
+    compare_method("adam+warmup", name="adam+warmup, bs=32", batch_size=32,
+        data_seed=data_seed, n_samples=n_samples, class_sep=class_sep)
+
+def test_adam_warmup2():
+    data_seed=1; n_samples=100; class_sep=2
+    compare_method("adam+warmup", name="adam+warmup, bs=32", batch_size=32,
+        data_seed=data_seed, n_samples=n_samples, class_sep=class_sep)
+
+def test_adam_warmup3():
+    data_seed=1; n_samples=500; class_sep=0.5; n_features = 100
+    compare_method("adam+warmup", name="adam+warmup, bs=32", batch_size=32,
+        data_seed=data_seed, n_samples=n_samples, class_sep=class_sep, n_features=n_features)
