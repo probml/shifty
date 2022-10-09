b17bdf957e8c86de85c49947049e108a9546f234
Kevin Murphy <murphyk@gmail.com>
2022-10-09 04:40:57 +0000

    start to refactor tta to use skax

--- shifty/tta/tta.py
+++ shifty/tta/tta.py
@@ -5,11 +5,8 @@ import einops
 import matplotlib
 from functools import partial
 from collections import namedtuple
+import dataclasses
 
-import sklearn
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
-from sklearn.pipeline import make_pipeline, Pipeline
-from sklearn.linear_model import LogisticRegression
 
 import jax
 import jax.random as jr
@@ -20,83 +17,14 @@ import chex
 import typing
 
 from tensorflow_probability.substrates import jax as tfp
-
 tfd = tfp.distributions
 from tensorflow_probability.substrates.jax.distributions import MultivariateNormalFullCovariance as MVN
 
-# Model from NURD paper:
-# A. M. Puli, L. H. Zhang, E. K. Oermann, and R. Ranganath, 
-# “Out-of-distribution Generalization in the Presence of Nuisance-Induced Spurious Correlations,” 
-#  ICLR, May 2022 [Online]. Available: https://openreview.net/forum?id=12RoR2o32T.
-
-
-@chex.dataclass
-class NurdParams:
-    b: float = 1 # strength of dependence of x on z (nuisance factor)
-    sf: float = 1 # scale factor for Gaussian variance
-    #generator: str = "nurd"
-
-@chex.dataclass
-class DiscrimParams:
-    poly_degree: int = 2
-    max_iter: int = 100
-
-@chex.dataclass
-class MetaParamsOld: # these determine the sizeshape of the model
-    ndim: float = 2 # dimensionality of generated X data
-    nclasses: int = 2
-    nfactors: int = 2 # other attributes
-    nmix: int = 4 # nclasses * nfactors
 
 nclasses = 2
 nfactors = 2
 nmix = 4
 
-@partial(jax.jit)
-def class_cond_params_nurd(z, nurd_params):  
-    # returns parameters for distribution p(x|m=(y,a))
-    ya = jnp.unravel_index(z, (nclasses, nfactors))
-    y, a = ya[0], ya[1]
-    b, sf = nurd_params.b, nurd_params.sf
-    ysigned, asigned = 2.0*y-1,  2.0*a-1 #   # convert from (0,1) to (-1,1)
-    mu = jnp.array([ysigned - b*asigned, ysigned + b*asigned])
-    Sigma = sf*jnp.diag(jnp.array([1.5, 0.5]))
-    return mu, Sigma
-
-@partial(jax.jit)
-def lik_fn_nurd(z, X, nurd_params):
-    # returns p(X(n,:) | z) as (N,1) vector
-    mu, Sigma = class_cond_params_nurd(z, nurd_params)
-    return jsp.stats.multivariate_normal.pdf(X, mu, Sigma)
-
-@partial(jax.jit, static_argnames=["nsamples"])
-def sample_data_nurd(key, nsamples, prior, nurd_params):
-    labels = jr.categorical(key, logits=jnp.log(prior), shape=(nsamples,))
-    def f(z):
-        return class_cond_params_nurd(z, nurd_params)
-    mus, Sigmas = vmap(f)(jnp.arange(nmix))
-    X = jr.multivariate_normal(key, mus[labels], Sigmas[labels])
-    return X, labels
-
-@partial(jax.jit, static_argnames=["nsamples"])
-def data_generator_nurd(key, nsamples, prior, nurd_params):
-    XX, ZZ = sample_data_nurd(key, nsamples, prior, nurd_params)
-    def lik_fn(z, X):
-        return lik_fn_nurd(z, X, nurd_params)
-    #ypost, zpost = predict_bayes(prior, lik_fn, XX)
-    ypost, zpost = predict_bayes(prior, partial(lik_fn_nurd, nurd_params=nurd_params), XX)
-    return XX, ZZ, ypost
-
-@partial(jax.jit, static_argnames=["lik_fn"])
-def predict_bayes(prior, lik_fn, X):  
-    liks = vmap(partial(lik_fn, X=X))(jnp.arange(nmix)) # liks(k,n)=p(X(n,:) | z=k)
-    joint = jnp.einsum('kn,k -> nk', liks, prior) # joint(n,k) = liks(k,n) * prior(k)
-    norm = joint.sum(axis=1)
-    zpost = joint / jnp.expand_dims(norm, axis=1) # zpost(n,k) = p(z) = k | xn)
-    ypost = compute_class_post_from_zpost(zpost)
-    return ypost, zpost
-
-@partial(jax.jit)
 def make_prior(rho):
     p_class =  jnp.array([0.5, 0.5]) # uniform prior  on p(y) = (-1, +1)
     p_factor_given_class = jnp.array([ [1-rho, rho], [rho, 1-rho]])
@@ -104,28 +32,51 @@ def make_prior(rho):
     pmix = einops.rearrange(pmix, 'y a -> (y a)')
     return pmix
 
-@partial(jax.jit)
+@chex.dataclass
+class GenParams:
+    nclasses: int
+    nfeatures: int
+    prior: chex.Array
+    mus: chex.Array # (C,D)
+    Sigmas: chex.Array #(C,D,D)
+
+def make_params(key, nclasses, nfeatures, prior=None, scale_factor=1):
+    mus = jr.normal(key, (nclasses, nfeatures)) # (C,D)
+    # shared covariance -> linearly separable
+    #Sigma = scale_factor * jnp.eye(nfeatures)
+    #Sigmas = jnp.array([Sigma for _ in range(nclasses)]) # (C,D,D)
+    # diagonal covariance -> nonlinear decision boundaries
+    sigmas = jr.uniform(key, shape=(nclasses, nfeatures), minval=0.5, maxval=5)
+    Sigmas = jnp.array([scale_factor*jnp.diag(sigmas[y]) for y in range(nclasses)])
+    if prior is None:
+        prior = jnp.ones(nclasses)/nclasses
+    return GenParams(nclasses=nclasses, nfeatures=nfeatures, prior=prior, mus=mus, Sigmas=Sigmas)
+
+def sample_data(key, params, nsamples):
+    y = jr.categorical(key, logits=jnp.log(params.prior), shape=(nsamples,))
+    X = jr.multivariate_normal(key, params.mus[y], params.Sigmas[y])
+    return X, y
+
+def predict_bayes(X, params):
+    def lik_fn(y):
+        return   jsp.stats.multivariate_normal.pdf(X, params.mus[y], params.Sigmas[y])
+    liks = vmap(lik_fn)(jnp.arange(params.nclasses)) # liks(k,n)=p(X(n,:) | y=k)
+    joint = jnp.einsum('kn,k -> nk', liks, params.prior) # joint(n,k) = liks(k,n) * prior(k)
+    norm = joint.sum(axis=1) # norm(n)  = sum_k joint(n,k) = p(X(n,:)
+    post = joint / jnp.expand_dims(norm, axis=1) # post(n,k) = p(y = k | xn)
+    return post
+
+
 def compute_class_post_from_zpost(z_post):
     z_post = einops.rearrange(z_post, 'n (y a) -> n y a', y=nclasses, a=nfactors)
     class_post = einops.reduce(z_post, 'n y a -> n y', 'sum') 
     return class_post
 
-
-def fit_classifier(key, X, Z):
-    classifier = Pipeline([
-            ('standardscaler', StandardScaler()),
-            ('poly', PolynomialFeatures(degree=2)), 
-            ('logreg', LogisticRegression(random_state=0, max_iter=100))])
-    classifier.fit(np.array(X), np.array(Z))
-    return classifier
-
-@partial(jax.jit)
 def predict_classifier(classifier, X):
-    zpost = jnp.array(classifier.predict_proba(X)) # (N,Z)
+    zpost = classifier.predict(X) # (N,Z)
     ypost = compute_class_post_from_zpost(zpost)
     return ypost, zpost
 
-@partial(jax.jit)
 def classifier_to_lik_fn(classifier, prior):
     def lik_fn(z, X):
         # return p_s(x(n) | z) = p_s(z|x) p_s(x) / p_s(z) propto p_s(z|x) / p_s(z)
@@ -134,7 +85,6 @@ def classifier_to_lik_fn(classifier, prior):
         return probs
     return lik_fn
 
-@partial(jax.jit)
 def em(X, init_dist, lik_fn):
   target_dist = init_dist
   pseudo_counts = 0.01 * init_dist
@@ -152,11 +102,6 @@ def em(X, init_dist, lik_fn):
 def mse(u, v):
   return jnp.mean(jnp.power(u-v, 2))
 
-
-@chex.dataclass
-class SourceModelOld:
-    prior: typing.Any 
-    classifier: typing.Any 
 
 @partial(jax.jit)
 def target_predict_unadapted(key, X, source_prior, classifier, target_prior):
@@ -179,42 +124,59 @@ def target_fit_em(key, X, source_prior, classifier):
     target_prior = em(X, source_prior, lik_fn)
     return target_prior
 
-def source_fit_classifier(key, X, Z):
-    classifier = fit_classifier(key, X, Z)
-    probsY, probsZ = predict_classifier(classifier, X)
-    priorZ = jnp.mean(probsZ, axis=0) # prior(mz = empirical fraction of times z is predicted
-    return priorZ, classifier
 
-@partial(jax.jit)
-def eval_single_target(key_base, corr_target, data_generator_fn, source_prior, classifier,
-                        target_fit_fn, target_predict_fn):
-    prior_target_true = make_prior(corr_target)
+def eval_single_target(key_base, corr_target, classifier, target_fit_fn, source_prior_est, params_source):
+    params_target = dataclasses.replace(params_source)
+    params_target.prior = make_prior(corr_target)
     nsamples_target = 500
-    key, key_base = jr.split(key_base, 2)
-    X, Z, Yprobs_true = data_generator_fn(key, nsamples_target, prior_target_true)
-    prior_target_est = target_fit_fn(key, X, source_prior, classifier)
+    key, subkey = jr.split(key, 2)
+    X, Z = sample_data(subkey, params_target, nsamples_target)
+    prior_target_est = target_fit_fn(key, X, source_prior_est, classifier)
+
     Yprobs_pred = target_predict_fn(key, X, source_prior, classifier, prior_target_est)
     mse_probs = mse(Yprobs_true[:,0], Yprobs_pred[:,0])
     mse_prior = mse(prior_target_true, prior_target_est)
     return jnp.array([mse_probs, mse_prior])
 
-def eval_multi_target(key_base, corr_source, corr_targets, data_generator_fn, source_fit_fn,
-                    target_fit_fn, target_predict_fn):
-    prior_source = make_prior(corr_source)
+def eval_multi_target(key, corr_source, corr_targets, classifier, target_fit_fn):
+    prior = make_prior(corr_source)
+    nclasses = len(prior)
+    key, subkey = jr.split(key, 2)
+    gen_params = make_params(subkey, nclasses, nfeatures=10, prior=prior, scale_factor=1)
     nsamples_source = 500
-    key, key_base = jr.split(key_base, 2)
-    X, Z, _ = data_generator_fn(key, nsamples_source, prior_source)
-    key, key_base = jr.split(key_base, 2)
-    source_prior, classifier = source_fit_fn(key, X, Z)
-    key, key_base = jr.split(key_base, 2)
-    def f(corr_target):
-        return eval_single_target(key, corr_target, data_generator_fn, source_prior, classifier,
-                            target_fit_fn, target_predict_fn)
-    losses = vmap(f)(corr_targets)
-    #losses = []
-    #for i, corr_target in enumerate(corr_targets):
-    #    loss = f(corr_target)
-    #    print(loss)
-    #    losses.append(loss)
+    key, subkey = jr.split(key, 2)
+    X, Z = sample_data(subkey, gen_params, nsamples_source)
 
+    key, subkey = jr.split(key, 2)
+    classifier.fit(subkey, X, Z)
+    probsZ = classifier.predict(X)
+    priorZ = jnp.mean(probsZ, axis=0) # prior(mz = empirical fraction of times z is predicted
+
+    ntargets = len(corr_targets)
+    keys = jr.split(key, ntargets)
+    def f(key, corr_target):
+        return eval_single_target(key, corr_target, classifier, target_fit_fn, priorZ, gen_params)
+    losses = vmap(f)(zip(keys, corr_targets))
     return losses
+
+'''
+def demo():
+    key = jr.PRNGKey(0)
+    corr_source = 0.1; corr_targets = jnp.arange(0.1, 0.9, step=0.1)
+    losses_unadapted = eval_multi_target(key, corr_source, corr_targets)
+    print(losses_unadapted)
+
+    source_fit_fn = source_fit_classifier
+    target_fit_fn  = target_fit_em
+    target_predict_fn = target_predict_bayes
+
+    losses_em = eval_multi_target(key, corr_source, corr_targets,
+        data_generator_fn, source_fit_fn, target_fit_fn, target_predict_fn, meta_params)
+    print(losses_em)
+
+    plt.figure;
+    plt.plot(corr_targets, losses_em[:,0], 'r-', label='em')
+    plt.plot(corr_targets, losses_unadapted[:,0], 'k:', label='unadapted')
+    plt.xlabel('correlation')
+    plt.ylabel('mse probs')
+'''
