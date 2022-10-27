0f04d1f0b682e7491bcbe1f0db881976729da936
Kevin Murphy <murphyk@gmail.com>
2022-10-27 17:02:17 +0000

    checkin from VM

--- shifty/tta/data_generator.py
+++ shifty/tta/data_generator.py
@@ -125,3 +125,68 @@ class NurdDataGenerator(GMMDataGenerator):
         flat_prior = make_correlated_prior(self.correlation, self.class_prior)
         self.params = GMMParams(nmix=nmix, nfeatures=nfeatures, prior=flat_prior, mus=mus, Sigmas=Sigmas)
 
+###
+
+# We "plant" two different vectors into feature space, depending on label y and attributes a
+
+
+def class_cond_params_plant(z, sf):  
+    # returns parameters for distribution p(x|m=(y,a))
+    nclasses, nfactors = 2, 2
+    ya = jnp.unravel_index(z, (nclasses, nfactors))
+    y, a = ya[0], ya[1]
+    mu_y = jnp.array([-1, 1])
+    mu_a = jnp.array([-1, 1])
+    mu = jnp.array([mu_y[y], mu_a[a]]) # extract appropriate elements for each block of the vector
+    Sigma = jnp.diag(jnp.array([sf*0.1, 0.1])) # ensure that label features y are noisier than attribute features a 
+    return mu, Sigma
+
+
+class PlantDataGenerator(GMMDataGenerator):
+    def __init__(self, key, correlation, label_space, sf=5):
+        # key is used to generate random GMM parameters in parent, but these are overwritten with deterministic values
+        self.correlation = correlation
+        assert label_space.nclasses == 2
+        assert label_space.nfactors == 2
+        nclasses, nfactors, nmix = 2, 2, 4
+        self.class_prior = jnp.array([0.5,0.5])
+        nfeatures = 2 # 2d
+        super().__init__(key, correlation, label_space, nfeatures, class_prior=self.class_prior, noise_factor=1)
+        def f(z):
+            return class_cond_params_plant(z,  sf)
+        mus, Sigmas = vmap(f)(jnp.arange(nmix))
+        flat_prior = make_correlated_prior(self.correlation, self.class_prior)
+        self.params = GMMParams(nmix=nmix, nfeatures=nfeatures, prior=flat_prior, mus=mus, Sigmas=Sigmas)
+
+
+###
+
+def class_cond_params_plant_multidim(z, label_noise, attribute_noise, label_ndims, attribute_ndims):  
+    # returns parameters for distribution p(x|m=(y,a))
+    nclasses, nfactors = 2, 2
+    ya = jnp.unravel_index(z, (nclasses, nfactors))
+    y, a = ya[0], ya[1]
+    mu_y = jnp.stack([-1*jnp.ones(label_ndims), +1*jnp.ones(label_ndims)]) # label_dims,2
+    mu_a = jnp.stack([-1*jnp.ones(attribute_ndims), +1*jnp.ones(attribute_ndims)]) # label_dims,2
+    mu = jnp.array([mu_y[y], mu_a[a]]) # extract appropriate elements for each block of the vector
+    sigma_y = label_noise*jnp.ones(label_ndims)
+    sigma_a = attribute_noise*jnp.ones(attribute_ndims)
+    sigma = jnp.concatenate([sigma_y, sigma_a])
+    Sigma = jnp.diag(sigma)
+    return mu, Sigma
+
+class PlantDataGeneratorMultiDim(GMMDataGenerator):
+    def __init__(self, key, correlation, label_space, label_noise=5, attribute_noise=1, label_ndims=1, attribute_ndims=1):
+        # key is used to generate random GMM parameters in parent, but these are overwritten with deterministic values
+        self.correlation = correlation
+        assert label_space.nclasses == 2
+        assert label_space.nfactors == 2
+        nclasses, nfactors, nmix = 2, 2, 4
+        self.class_prior = jnp.array([0.5,0.5])
+        nfeatures = label_ndims + attribute_ndims
+        super().__init__(key, correlation, label_space, nfeatures, class_prior=self.class_prior, noise_factor=1)
+        def f(z):
+            return class_cond_params_plant_multidim(z, label_noise, attribute_noise, label_ndims, attribute_ndims)
+        mus, Sigmas = vmap(f)(jnp.arange(nmix))
+        flat_prior = make_correlated_prior(self.correlation, self.class_prior)
+        self.params = GMMParams(nmix=nmix, nfeatures=nfeatures, prior=flat_prior, mus=mus, Sigmas=Sigmas)
--- shifty/tta/tta.py
+++ shifty/tta/tta.py
@@ -22,7 +22,56 @@ from shifty.tta.data_generator import *
 from shifty.tta.estimators import *
 from shifty.skax.skax import *
 
-def run_expt(corr_source=0.3, ntrials=3, sf=3):
+def run():
+    key = jr.PRNGKey(42)
+    ls = LabelSpace(nclasses=2, nfactors=2)
+    sf = 3
+    def make_dist(key, rho):
+        src_dist = NurdDataGenerator(key, rho, ls, sf=sf)
+        return src_dist
+    corr_sources = jnp.array([0.2, 0.5, 0.9])
+    corr_targets = np.linspace(0.05, 0.95, num=10)
+    ntrials = 5
+    nsource_samples = 500
+    ntarget_samples = 100
+    clf = make_logreg
+
+
+    methods = {
+    'oracle': OracleEstimator(ls),
+    'oracle-prior': OraclePriorEstimator(clf, ls),
+    'unadapted': UndaptedEstimator(clf, ls),
+    'em': EMEstimator(clf, ls)
+    }
+
+    losses_mean = {}
+    losses_std = {}
+    for name, estimator in methods.items():
+        print(name)
+        losses_mean[name], losses_std[name] = evaluate_estimator(key, make_dist,
+            corr_sources, corr_targets, ntrials, estimator, nsource_samples = 500, ntarget_samples=100)
+
+    print(losses_mean['unadapted'].shape) # (nsources, ntargets, nmetrics)
+
+    nsources = len(corr_sources)
+    metric_names = ['misclassification', 'mse', '1-auc']
+    nmetrics = len(metric_names)
+    fig, axs = plt.subplots(nmetrics, nsources, figsize=(20,10))
+    fig.tight_layout(pad=3.0)
+    for m in range(nmetrics):
+        for s in range(nsources):
+            ax = axs[m,s]
+            corr_source = corr_sources[s]
+            for name in methods.keys():
+                ax.errorbar(corr_targets, losses_mean[name][s,:,m], yerr=losses_std[name][s,:,m], marker='o', label=name)
+            ax.set_xlabel('correlation')
+            ax.set_ylabel(metric_names[m])
+            ax.set_title(r'Source sf={:0.1f}, $\rho={:0.1f}$'.format(sf, corr_source))
+            ax.legend()
+            ax.axvline(x=corr_source)
+
+
+def run_nurd(corr_source=0.3, ntrials=3, sf=3):
     corr_targets = np.linspace(0.1, 0.9, num=9)
 
     key = jr.PRNGKey(420)
@@ -58,3 +107,41 @@ def run_expt(corr_source=0.3, ntrials=3, sf=3):
     plt.title(r'Source $\rho={:0.1f}$'.format(corr_source))
     plt.legend()
     plt.axvline(x=corr_source);
+
+def debug_auc():
+    key = jr.PRNGKey(42)
+    corr_source = 0.3
+    ls = LabelSpace(nclasses=2, nfactors=2)
+    src_dist = PlantDataGenerator(key, corr_source, ls, sf=5)
+    Xs, Ys, As = src_dist.sample(key, 500)
+    clf = make_logreg
+    estimator = UndaptedEstimator(clf, ls)
+    #estimator = EMEstimator(clf, ls)
+
+    estimator.fit_source(Xs, Ys, As, src_dist)
+
+    corr = 0.9
+    target_dist = deepcopy(src_dist)
+    target_dist.shift_prior_correlation(corr)
+    Xt, Yt, At = target_dist.sample(key, 100)
+    true_prob = target_dist.predict_class(Xt)
+    estimator.fit_target(Xt, target_dist)
+    pred_prob = estimator.predict_class_target(Xt)
+
+    y_test  = Yt
+    prob_vector = pred_prob[:,1]
+
+
+    fpr_sklearn, tpr_sklearn, thresholds_sklearn = sklearn.metrics.roc_curve(y_test, prob_vector)
+    auc_sklearn = sklearn.metrics.roc_auc_score(y_test, prob_vector)
+    fpr_kpm, tpr_kpm, thresholds_kpm = roc(y_test, prob_vector)
+    auc_kpm = roc_auc(y_test, prob_vector)
+
+
+    plt.figure()
+    plt.scatter(fpr_sklearn, tpr_sklearn, s=100, alpha=0.5, color="blue", label="Scikit-learn")
+    plt.scatter(fpr_kpm, tpr_kpm, color="red", s=100, alpha=0.3, label="Our implementation")
+    plt.title("ROC Curve: AUC sklearn {:.3f}, us {:.3f}".format(auc_sklearn, auc_kpm))
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.legend()
