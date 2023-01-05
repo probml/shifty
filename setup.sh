08dd7ffeec2842f52ad2a55c324bca339b8b0c46
Kevin Murphy <murphyk@gmail.com>
2023-01-05 17:35:53 +0000

    setup from paperspace

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Setup git
+git config --global user.email "murphyk@gmail.com"
+git config --global user.name "Kevin Murphy"
+
+# jax
+pip install optax
+#pip install distrax
+pip install flax
+pip install einops
